6dae545fe96f13921e797adaed7677fddad5c887
Dmitry Pavlov <dpavlov@ipa.nw.ru>
2014-12-12 15:40:45 +0400

    specified dependencies to data-lib and draw-lib

--- info.rkt
+++ info.rkt
@@ -4,6 +4,8 @@
 
 (define deps '("base"
                "gui"
+               "draw-lib"
+               "data-lib"
                "table-panel"))
 
 (define pkg-desc "This library provides spreadsheet-editor% class.")
